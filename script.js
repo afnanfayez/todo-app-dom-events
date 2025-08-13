5851e0bdef1d1966e08bd8f9f96f03e39cace574
afnan <afnan232003@gmail.com>
2025-08-13 13:43:03 +0300

    Adding checked section

--- script.js
+++ script.js
@@ -4,20 +4,37 @@ const addTaskButton=document.getElementById("addTaskButton");
 const taskList=document.getElementById("taskList");
 
 addTaskButton.addEventListener("click",function(event){
-    if (taskInput.value.trim() !=='') {
+     const value = taskInput.value.trim();  
+    if (value !== '') {
+
         const check=document.createElement("input");
+        const check2=document.createElement("span");
         check.type="radio";
+        
+        check2.innerHTML="<i class='fa-solid fa-circle-check'></i>"
+        check2.classList.add("checkIcon");
+
      const task = document.createElement("li");
      task.textContent=taskInput.value;
       task.prepend(check);
+      task.prepend(check2);
+
 
       const deleteButton=document.createElement("button");
       deleteButton.innerHTML="<i class='fa-solid fa-trash-can'></i>";
         deleteButton.id="deleteButton";
       task.append(deleteButton);
      taskList.append(task);
-      //console.log("Task added:", task.textContent);
+   
+      console.log(task);
 taskInput.value = ''; 
+
+    check.addEventListener("click",function(){   
+    check.style.opacity="0";
+     check2.style.display="block";
+    });
+
+    
     }
-        
+
 });
