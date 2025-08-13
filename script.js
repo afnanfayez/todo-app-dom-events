78fcae4e4e08f8bb71c49caf4a46d14e794d308b
afnan <afnan232003@gmail.com>
2025-08-13 14:08:40 +0300

    Adding Delete Action And change Js

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskList = document.getElementById("taskList");
 
-const taskInput=document.getElementById("taskInput");
-const addTaskButton=document.getElementById("addTaskButton");
-const taskList=document.getElementById("taskList");
+function addTask() {
+  const value = taskInput.value.trim();
+  if (value !== "") {
+    const check = document.createElement("input");
+    const check2 = document.createElement("span");
+    check.type = "radio";
 
-addTaskButton.addEventListener("click",function(event){
-     const value = taskInput.value.trim();  
-    if (value !== '') {
+    check2.innerHTML = "<i class='fa-solid fa-circle-check'></i>";
+    check2.classList.add("checkIcon");
 
-        const check=document.createElement("input");
-        const check2=document.createElement("span");
-        check.type="radio";
-        
-        check2.innerHTML="<i class='fa-solid fa-circle-check'></i>"
-        check2.classList.add("checkIcon");
+    const task = document.createElement("li");
+    task.textContent = taskInput.value;
+    task.prepend(check);
+    task.prepend(check2);
 
-     const task = document.createElement("li");
-     task.textContent=taskInput.value;
-      task.prepend(check);
-      task.prepend(check2);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+    deleteButton.id = "deleteButton";
+    task.append(deleteButton);
 
+    taskList.append(task);
 
-      const deleteButton=document.createElement("button");
-      deleteButton.innerHTML="<i class='fa-solid fa-trash-can'></i>";
-        deleteButton.id="deleteButton";
-      task.append(deleteButton);
-     taskList.append(task);
-   
-      console.log(task);
-taskInput.value = ''; 
-
-    check.addEventListener("click",function(){   
-    check.style.opacity="0";
-     check2.style.display="block";
-      task.style.textDecoration="line-through";
+    check.addEventListener("click", function () {
+      check.style.opacity = "0";
+      check2.style.display = "block";
+      task.style.textDecoration = "line-through";
+      task.style.opacity = "0.5";
     });
 
+    deleteButton.addEventListener("click", function () {
+      task.remove();
+    });
 
-    }
+    taskInput.value = "";
+  }
+}
 
+addTaskButton.addEventListener("click", addTask);
+
+taskInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTask();
+  }
 });
